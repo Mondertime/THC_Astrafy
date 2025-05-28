2160a855826c4903c030ecc162f935a46d4055fe
Mondertime <mondertime@gmail.com>
2025-05-28 16:09:04 +0200

    add staging models

---
+++ thc_astrafy/models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+-- Cleaned staging model for orders table
+SELECT
+	FORMAT_DATE('%Y-%m-%d', date_date)  AS order_date,
+	CAST(customers_id AS STRING) AS customer_id,
+	CAST(orders_id AS STRING) AS order_id,
+	CAST(net_sales AS FLOAT64) AS net_sales
+FROM 
+	{{ source('raw', 'orders') }}
---
+++ thc_astrafy/models/staging/stg_sales.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+-- Cleaned staging model for sales table
+SELECT
+	FORMAT_DATE('%Y-%m-%d', date_date) AS sale_date,
+	CAST(customer_id AS STRING) AS customer_id,
+	CAST(order_id AS STRING) AS order_id,
+	CAST(products_id AS STRING) AS product_id,
+	CAST(net_sales AS FLOAT64) AS net_sales,
+	CAST(qty AS INT64) AS quantity
+FROM 
+	{{ source('raw', 'sales') }}
